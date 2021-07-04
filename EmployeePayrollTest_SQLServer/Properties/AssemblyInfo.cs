3c9a5f79edd2f9a9ac59857961f7a2849f2e9aff
pampapathi73 <pampapathik32@gmail.com>
2021-07-04 08:04:06 +0530

    [pampapathi] Added: Employee payroll

---
+++ EmployeePayrollTest_SQLServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeePayrollTest_SQLServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeePayrollTest_SQLServer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2cf6b872-0572-4688-adf4-4702c74ce9d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
